567151a71643440a59c38844ac95c24e2f468433
GaMaur Landrum <gamaur.landrum@gmail.com>
2016-01-06 11:57:28 -0800

    added youtube link

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+
+  def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  # private_params
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :text, :image_uri, :tags, :likes, :youtube_embed_id)
+  end
+
+end
---
+++ db/migrate/20160106190515_removeimage_uri_from_posts.rb
@@ -0,0 +1,4 @@
+class RemoveimageUriFromPosts < ActiveRecord::Migration
+  def change
+  end
+end
