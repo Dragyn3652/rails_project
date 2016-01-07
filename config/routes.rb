346cb4dfbc81589cbb6a6a318b4cd1d4f5e4dbf1
GaMaur Landrum <gamaur.landrum@gmail.com>
2016-01-06 16:08:01 -0800

    homepage is displaying posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'posts#index'
 
   resources :users, only: [:new, :create, :show, :edit, :update] do
     resources :posts, only: [:index, :create]
